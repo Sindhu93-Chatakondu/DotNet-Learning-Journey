3b71c9df2752d2ab2c092328217a344af6fd66ac
suprajasindhu <c.h.suprajasindhu35@gmail.com>
2026-07-22 12:24:02 +0530

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sindhu";
+            int age = 23;
+            double cgpa = 7.59;
+            bool isGraduate = true;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"CGPA: {cgpa}");
+            Console.WriteLine($"Graduate: {isGraduate}");
+        }
+    }
+}
